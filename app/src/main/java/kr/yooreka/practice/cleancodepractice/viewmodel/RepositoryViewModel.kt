9693418cc34aff9286d99e12e809c0f9ae26be95
Marihuana <Bracket0723@gmail.com>
2021-10-31 23:45:57 +0900

    Di 작성 EntryPoint 작성 Module 및 provide 작성

--- app/src/main/java/kr/yooreka/practice/cleancodepractice/viewmodel/RepositoryViewModel.kt
+++ app/src/main/java/kr/yooreka/practice/cleancodepractice/viewmodel/RepositoryViewModel.kt
@@ -1,8 +1,10 @@
 package kr.yooreka.practice.cleancodepractice.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kr.yooreka.practice.domain.usecase.ReposUseCase
 
+@HiltViewModel
 class RepositoryViewModel(val useCase : ReposUseCase) : ViewModel() {
 
 
